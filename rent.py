dd2cd48116146d482d44b3635964cd576c40da7f
Incipiens <adamconway99@gmail.com>
2023-05-09 10:49:56 +0100

    Updated rent.py

--- rent.py
+++ rent.py
@@ -1,12 +1,10 @@
-from bs4 import BeautifulSoup
 import requests
+import difflib
 import time
 
 def get_website_contents(url, headers):
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    # Extract the text content of the website
-    return soup.get_text()
+    return response.content.decode("utf-8")
 
 def check_for_changes(url, headers, interval):
     old_contents = get_website_contents(url, headers)
@@ -17,15 +15,16 @@ def check_for_changes(url, headers, interval):
         except requests.exceptions.RequestException as e:
             print("Error occurred:", e)
             continue
-        if old_contents != new_contents:
+        similarity = difflib.SequenceMatcher(None, old_contents, new_contents).ratio()
+        if similarity < 0.95:
             print("Website has been updated!")
             old_contents = new_contents
         else:
             print("Website has not been updated.")
 
-url = "https://www.daft.ie/property-for-sale/south-co-dublin-dublin?sort=publishDateDesc"
+url = "https://example.com"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 }
-interval = 5 # seconds
+interval = 60 # seconds
 check_for_changes(url, headers, interval)
